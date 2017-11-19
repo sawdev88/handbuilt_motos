72b10cda728dab2f719846cd81491e02aa58c32f
Sean Wines <sean.wines@vuria.com>
2017-11-19 13:09:07 -0800

    Created initial pages and routes

---
+++ app/controllers/pages_controller.rb
@@ -0,0 +1,13 @@
+class PagesController < ApplicationController
+  def index
+  end
+
+  def home
+  end
+
+  def garage_listings
+  end
+
+  def profile
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,13 @@
 Rails.application.routes.draw do
+  
+  # define root url
+  root 'pages#index'
+
+  get '/home' => 'pages#home'
+
+  get '/garage-listings' => 'pages#garage-listings'
+
+  get '/profile' => 'pages#profile'
+
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
