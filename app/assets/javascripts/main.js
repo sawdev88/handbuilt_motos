66b491d62868a25f6d9854bfa1b1bc3888ea55f3
Sean Wines <sean.wines@vuria.com>
2017-11-19 16:35:54 -0800

    Created user nav

--- app/assets/javascripts/main.js
+++ app/assets/javascripts/main.js
@@ -1,8 +1,12 @@
 var ready = function() {
   // Toggle drawer
-  $('.handle-drawer').on('click', function () {
-    $('#drawer').toggleClass('show-drawer');
-    $(this).toggleClass('fa-bars fa-close')
+  // $('.handle-drawer').on('click', function () {
+  //   $('#drawer').toggleClass('show-drawer');
+  //   $(this).toggleClass('fa-bars fa-close')
+  // })
+  // Profile settings nav
+  $('.avatar-image').on('click', function () {
+    $('.profile-settings-list').fadeToggle();
   })
 };
 
